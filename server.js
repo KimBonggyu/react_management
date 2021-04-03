2c26d779ac94b3ef9f54b4273ac02486d337570a
KimBonggyu <kbgkbg1999@gmail.com>
2021-04-03 22:01:56 +0900

    7th commit - connect mariadb - need fix table data

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,33 +7,27 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+});
+
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-            'id':1,
-            'image':'http://placeimg.com/64/64/1',
-            'name' : '김봉규',
-            'birthday' : '910812',
-            'gender' : '남자',
-            'job' : '무직'
-          },
-          {
-            'id':2,
-            'image':'http://placeimg.com/64/64/2',
-            'name' : '노라녀',
-            'birthday' : '980404',
-            'gender' : '여자',
-            'job' : '기자'
-          },
-          {
-            'id':3,
-            'image':'http://placeimg.com/64/64/3',
-            'name' : '아라미',
-            'birthday' : '921203',
-            'gender' : '여자',
-            'job' : '학생'
-          } 
-    ]);
+  connection.query(
+    "select * from tbl_customer",
+    (err, rows, fields) => {
+      res.send(rows);
+    }
+  );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
